69bef54c27531078ef7b3f5dda40b52f03fd1cda
Uriel <ur13l.infante@gmail.com>
2019-12-02 15:49:31 -0600

    [UPDATE ] Indicators 100%

--- src/components/indicators.js
+++ src/components/indicators.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types"
 import React, { Component } from "react"
 import styled from "styled-components"
 import "../styles/global.css"
+import fetch from "cross-fetch"
 var yf = require("yahoo-finance")
 
 /**
@@ -102,87 +103,29 @@ const IndicatorsWrapper = styled.div`
 class Indicators extends Component {
   constructor() {
     super()
-    this.state ={indicators: []}
+    this.state = { indicators: [] }
   }
 
   componentDidMount() {
-    this.updateIndicators()
-    this.setState({
-      indicators: this.collectIndicators()
-    })
-  }
-  /**
-   * Indicators component
-   * @param {string} location
-   */
-  updateIndicators = () => {
-    if (localStorage) {
-      yf.quote(
-        { symbol: "MEX-TIIE28.MX", modules: ["price"] },
-        (err, quotes) => {
-          localStorage.tiie = JSON.stringify(quotes.price)
-        }
-      )
-      yf.quote(
-        { symbol: "MEX-CETES-I.MX", modules: ["price"] },
-        (err, quotes) => {
-          localStorage.cetes = JSON.stringify(quotes.price)
-        }
-      )
-
-      yf.quote({ symbol: "USDMXN=X", modules: ["price"] }, (err, quotes) => {
-        localStorage.usdmxn = JSON.stringify(quotes.price)
-      })
-      yf.quote({ symbol: "^MXX", modules: ["price"] }, (err, quotes) => {
-        localStorage.ipc = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "^NYA", modules: ["price"] }, (err, quotes) => {
-        localStorage.nyse = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "^IXIC", modules: ["price"] }, (err, quotes) => {
-        localStorage.nasdaq = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "EWW", modules: ["price"] }, (err, quotes) => {
-        localStorage.eww = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "SPY", modules: ["price"] }, (err, quotes) => {
-        localStorage.spy = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "BTC-USD", modules: ["price"] }, (err, quotes) => {
-        localStorage.btcusd = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "XRP-USD", modules: ["price"] }, (err, quotes) => {
-        localStorage.xrpusd = JSON.stringify(quotes.price)
-      })
-
-      yf.quote({ symbol: "ETH-USD", modules: ["price"] }, (err, quotes) => {
-        localStorage.ethusd = JSON.stringify(quotes.price)
-      })
-    }
+    this.collectIndicators()
   }
 
   collectIndicators = () => {
-    let ind = []
-    if (localStorage) {
-      if (localStorage.cetes) ind.push(JSON.parse(localStorage.cetes))
-      if (localStorage.tiie) ind.push(JSON.parse(localStorage.tiie))
-      if (localStorage.usdmxn) ind.push(JSON.parse(localStorage.usdmxn))
-      if (localStorage.ipc) ind.push(JSON.parse(localStorage.ipc))
-      if (localStorage.nyse) ind.push(JSON.parse(localStorage.nyse))
-      if (localStorage.nasdaq) ind.push(JSON.parse(localStorage.nasdaq))
-      if (localStorage.eww) ind.push(JSON.parse(localStorage.eww))
-      if (localStorage.spy) ind.push(JSON.parse(localStorage.spy))
-      if (localStorage.btcusd) ind.push(JSON.parse(localStorage.btcusd))
-      if (localStorage.xrpusd) ind.push(JSON.parse(localStorage.xrpusd))
-      if (localStorage.ethusd) ind.push(JSON.parse(localStorage.ethusd))
-    }
-    return ind
+    fetch("https://s3.us-east-2.amazonaws.com/hub.rocktech/prices.json")
+    .then(response => response.json())
+    .then(json => {
+      let array = []
+      const keys = Object.keys(json);
+      for(const key of keys) {
+        array.push(json[key]['price'])
+      }
+
+
+     this.setState({
+        indicators: array
+     }) 
+    });
+    return [];
   }
 
   render() {
