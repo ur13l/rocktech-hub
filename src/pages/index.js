784c320a51b55ec8c3807f9f739da07349d04e27
Mario Alberto Negrete Rodríguez <marioalbertonegreterodriguez@Marios-MacBook-Pro.local>
2020-07-31 13:02:08 -0500

    label added in index quiénes somos

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,39 @@ const LayoutWrapper = styled.div`
   min-height: 99%;
   color: white;
 
+  .phrase{
+    background-color: #FFF;
+    color: #000;
+    font-weight: bold;
+    padding: 7px;
+
+  }
+
+  .border{
+    border: solid 1px white;
+    padding: 5px;
+  }
+
+  .cf{
+    color: rgb(192,137,6);
+  }
+
+  .rd{
+    color: rgb(42,70,139);
+  }
+
+  .ec{
+    color: rgb(16,128,128);
+  }
+
+  .align-center{
+    text-align: center;
+    margin-bottom: 40px;
+  }
+
   .rocktech-def {
     padding-top: 6vh;
-    margin-bottom: 12vh;
+    margin-bottom: 10vh;
   }
 
   .react-multi-carousel-dot  {
@@ -47,6 +77,7 @@ const LayoutWrapper = styled.div`
   
   .def-rocktech {
     font-size: 24px;
+    margin-top: 10px !important;
     text-align: center;
   }
   
@@ -65,8 +96,14 @@ const IndexPage = () => (
       <SEO title="Rocktech Hub"/>
       <div className="container">
         <div className="rocktech-def">
+        <p className="def align-center">
+        <span className="phrase">Rocktech es una empresa de ingeniería de negocios que promueve la 
+        institucionalización, innovación y la inversión desde sus tres 
+        unidades de negocio; <b className="cf">finanzas corporativas</b>, <b className="rd">investigación + 
+        desarrollo</b> y <b className="ec">fondeo colectivo de capital</b>.</span>
+          </p>
           <p className="def-rocktech">
-          Perfeccionamos, Optimizamos y Ofertamos Buenos Negocios.
+          <span className="border">Perfeccionamos, Optimizamos y Ofertamos Buenos Negocios.</span>
           </p>
         </div>
         <div className="units">
